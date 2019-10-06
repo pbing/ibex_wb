9d0de6ca40d36a7263481c2e9a233841625809ba
Bernd Beuster <bernd.beuster@gmail.com>
2019-10-06 21:29:00 +0200

    Add word select for led module

--- sim/led/led.sv
+++ sim/led/led.sv
@@ -3,17 +3,22 @@
 `default_nettype none
 
 module led
+  #(parameter size)
   (output logic led,
    wb_if.slave  wb);
 
+   localparam addr_width = $clog2(size) - 2;
+
    logic valid;
+   logic select;
 
    always @(posedge wb.clk)
-     if (valid && wb.we)
+     if (valid && wb.we && select)
        led <= wb.dat_i[0];
 
    /* Wishbone control */
    assign valid    = wb.cyc & wb.stb;
+   assign select   = wb.adr[addr_width - 1 : 2] == 0;
    assign wb.stall = 1'b0;
    assign wb.err   = 1'b0;
 
