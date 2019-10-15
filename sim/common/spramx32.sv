bdb8c46b9760b80de7dd2b3b3c311b1b1b7cdf9c
Bernd Beuster <bernd.beuster@creativechips.com>
2019-10-15 17:11:37 +0200

    Improve RAM model

--- sim/common/spramx32.sv
+++ sim/common/spramx32.sv
@@ -3,20 +3,25 @@
 `default_nettype none
 
 module spramx32
-  #(parameter size,
+  #(parameter size       = 'h80,
     parameter addr_width = $clog2(size) - 2)
    (input  logic                    clk,  // clock
     input  logic [addr_width - 1:0] addr, // address
     input  logic                    ce,   // chip enable
-    input  logic                    we,   // write enable
+    input  logic [3:0]              we,   // write enables
     input  logic [31:0]             d,    // data input
     output logic [31:0]             q);   // data output
 
    logic [31:0] mem[size];
 
    always @(posedge clk)
-     if (ce && we)
-       mem[addr] <= d;
+     if (ce)
+       begin
+          if (we[0]) mem[addr][7:0]   <= d[7:0];
+          if (we[1]) mem[addr][15:8]  <= d[15:8];
+          if (we[2]) mem[addr][23:16] <= d[23:16];
+          if (we[3]) mem[addr][31:24] <= d[31:24];
+       end
 
    always_ff @(posedge clk)
      if (ce)
--- sim/common/wb_spramx32.sv
+++ sim/common/wb_spramx32.sv
@@ -3,26 +3,33 @@
 `default_nettype none
 
 module wb_spramx32
-  #(parameter size)
+  #(parameter size = 'h80)
    (wb_if.slave wb);
 
    localparam addr_width = $clog2(size) - 2;
 
-   logic [addr_width - 1:0] waddr;     // word address
    logic                    valid;
+   logic [addr_width - 1:0] ram_addr;     // RAM address
+   logic                    ram_ce;
+   logic [3:0]              ram_we;
+   logic [31:0]             ram_d;
    logic [31:0]             ram_q;
-
-   assign waddr = wb.adr[addr_width + 1 : 2];
 
    spramx32
      #(.size(size))
    spram
      (.clk  (wb.clk),
-      .addr (waddr),
-      .ce   (valid),
-      .we   (wb.we),
-      .d    (wb.dat_i),
+      .addr (ram_addr),
+      .ce   (ram_ce),
+      .we   (ram_we),
+      .d    (ram_d),
       .q    (ram_q));
+
+   assign ram_addr = wb.adr[addr_width + 1 : 2];
+   assign ram_ce   = valid;
+   assign ram_we   = {4{wb.we}} & wb.sel;
+   assign ram_d    = wb.dat_i;
+   assign wb.dat_o = ram_q;
 
    /* Wishbone control */
    assign valid    = wb.cyc & wb.stb;
@@ -34,8 +41,6 @@ module wb_spramx32
        wb.ack <= 1'b0;
      else
        wb.ack <= valid & ~wb.stall;
-
-   assign wb.dat_o = ram_q;
 endmodule
 
 `resetall
