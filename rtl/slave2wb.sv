a0b42d476fbfbf7a191d4c45a24a59acb0764140
Bernd Beuster <bernd.beuster@creativechips.com>
2019-11-03 09:14:18 +0100

    Fix typo

--- rtl/slave2wb.sv
+++ rtl/slave2wb.sv
@@ -6,20 +6,20 @@ module slave2wb
   (core_if.master slave,
    wb_if.slave    wb);
 
-   assign slave.req   = wbs.stb;
-   assign slave.we    = wbs.we;
-   assign slave.addr  = wbs.adr;
-   assign slave.be    = wbs.sel;
-   assign slave.wdata = wbs.dat_i;
-   assign wbs.dat_o   = slave.rdata;
-   assign wbs.stall   = 1'b0;
-   assign wbs.err     = 1'b0;
+   assign slave.req   = wb.stb;
+   assign slave.we    = wb.we;
+   assign slave.addr  = wb.adr;
+   assign slave.be    = wb.sel;
+   assign slave.wdata = wb.dat_i;
+   assign wb.dat_o   = slave.rdata;
+   assign wb.stall   = 1'b0;
+   assign wb.err     = 1'b0;
 
-   always_ff @(posedge wbs.clk or posedge wbs.rst)
-     if (wbs.rst)
-       wbs.ack <= 1'b0;
+   always_ff @(posedge wb.clk or posedge wb.rst)
+     if (wb.rst)
+       wb.ack <= 1'b0;
      else
-       wbs.ack <= wbs.cyc & wbs.stb;
+       wb.ack <= wb.cyc & wb.stb;
 endmodule
 
 `resetall
